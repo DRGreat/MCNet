9c5b2c5c79ba37a07e70f96a8f21f9a56649a559
chenderong <297772967@qq.com>
2022-03-25 22:16:12 +0800

    加入了data_augmented的方法，但是只进行了简单的拼接,效果显著，不借助data_aux比renet和ptoronet搞了一个多的百分点

--- datasets/download_cifar_fs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#download dataset
-#https://drive.google.com/file/d/1Lq2USoQmbFgCFJlGx3huFSfjqwtxMCL8/view?usp=sharing
-
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Lq2USoQmbFgCFJlGx3huFSfjqwtxMCL8' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Lq2USoQmbFgCFJlGx3huFSfjqwtxMCL8" -O cifar_fs.tar && rm -rf /tmp/cookies.txt
-
-tar -xvf cifar_fs.tar cifar_fs/
-
--- datasets/download_tieredimagenet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#download dataset
-#https://drive.google.com/file/d/1ANczVwnI1BDHIF65TgulaGALFnXBvRfs/view?usp=sharing
-
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ANczVwnI1BDHIF65TgulaGALFnXBvRfs' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ANczVwnI1BDHIF65TgulaGALFnXBvRfs" -O tieredimagenet.tar && rm -rf /tmp/cookies.txt
-
-tar -xvf tieredimagenet.tar tieredimagenet/
-
