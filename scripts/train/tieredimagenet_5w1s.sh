2c5abc5e66de29e806a8b7b2353ddcb8649c3240
chenderong <297772967@qq.com>
2022-04-20 14:12:50 +0800

    w

--- scripts/train/tieredimagenet_5w1s.sh
+++ scripts/train/tieredimagenet_5w1s.sh
@@ -1,3 +1,3 @@
 python train.py -batch 256 -dataset tieredimagenet -gpu 2 -extra_dir your_run -temperature_attn 5.0 -lamb 0.25 -no_wandb -lr 0.01 -max_epoch 10 -milestones 4 6 8
 
-nohup python train.py -batch 256 -dataset tieredimagenet -gpu 1 -extra_dir your_run -temperature_attn 5.0 -lamb 0.25 -no_wandb -lr 0.01 -max_epoch 10 -milestones 4 6 8 &
+nohup python train.py -batch 256 -dataset tieredimagenet -gpu 0 -extra_dir your_run -temperature_attn 5.0 -lamb 0.25 -no_wandb -lr 0.01 -max_epoch 10 -milestones 4 6 8 &
