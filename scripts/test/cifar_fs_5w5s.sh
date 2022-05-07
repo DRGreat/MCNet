f26a8e4c23524ed905a6047174860182961bf84a
chenderong <297772967@qq.com>
2022-05-07 10:43:59 +0800

    w

--- scripts/test/cifar_fs_5w5s.sh
+++ scripts/test/cifar_fs_5w5s.sh
@@ -1,2 +1,4 @@
 python test.py -dataset cifar_fs -gpu 1 -extra_dir renet_iccv21 -temperature_attn 5.0 -shot 5
 
+python test.py -dataset cifar_fs -gpu 1 -extra_dir your_run -temperature_attn 5.0 -shot 5 -no_wandb
+
