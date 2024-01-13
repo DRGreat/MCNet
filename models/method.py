cb709e4e24a8ffa105f89bec3104c4f941706989
DRGreat <297772967@qq.com>
2024-01-14 00:06:15 +0800

    m

--- models/method.py
+++ models/method.py
@@ -130,36 +130,36 @@ class Method(nn.Module):
 
         # ----------------------------------cat--------------------------------------#
 
-        spt_feats = spt.unsqueeze(0).repeat(num_qry, 1, 1).view(-1,*spt.size()[1:]) #shape of spt_feats [75x25, 9]
-        qry_feats = qry.unsqueeze(1).repeat(1, way, 1).view(-1,*qry.size()[1:]) #[75x25, 9]
-
-        corr = self.corr(self.l2norm(spt_feats), self.l2norm(qry_feats)).unsqueeze(1).repeat(1,1,1,1) #the shape of corr : [75x25,2, 9, 9]
-        spt_feats_proj = self.proj(spt_feats).unsqueeze(1).unsqueeze(2).repeat(1, 1, self.vit_dim, 1) #[75x25,2,9,3]
-        qry_feats_proj = self.proj(qry_feats).unsqueeze(1).unsqueeze(2).repeat(1, 1, self.vit_dim, 1) #[75x25,2,9,3]
-
-        refined_corr = self.decoder(corr, spt_feats_proj, qry_feats_proj).view(num_qry,way,*[self.feature_size]*4)
-        corr_s = refined_corr.view(num_qry, way, self.feature_size*self.feature_size, self.feature_size*self.feature_size)
-        corr_q = refined_corr.view(num_qry, way, self.feature_size*self.feature_size, self.feature_size*self.feature_size)
-        # normalizing the entities for each side to be zero-mean and unit-variance to stabilize training
-        corr_s = self.gaussian_normalize(corr_s, dim=2)
-        corr_q = self.gaussian_normalize(corr_q, dim=3)
-
-        # applying softmax for each side
-        corr_s = F.softmax(corr_s / self.args.temperature_attn, dim=2)
-        corr_q = F.softmax(corr_q / self.args.temperature_attn, dim=3)
-
-        # suming up matching scores
-        attn_s = corr_s.sum(dim=[3])
-        attn_q = corr_q.sum(dim=[2])
-
-        # applying attention
-        spt_attended = attn_s * spt_feats.view(num_qry, way, *spt_feats.shape[1:]) #[75, 25, 9]
-        qry_attended = attn_q * qry_feats.view(num_qry, way, *qry_feats.shape[1:]) #[75, 25, 9]
+        # spt_feats = spt.unsqueeze(0).repeat(num_qry, 1, 1).view(-1,*spt.size()[1:]) #shape of spt_feats [75x25, 9]
+        # qry_feats = qry.unsqueeze(1).repeat(1, way, 1).view(-1,*qry.size()[1:]) #[75x25, 9]
+        #
+        # corr = self.corr(self.l2norm(spt_feats), self.l2norm(qry_feats)).unsqueeze(1).repeat(1,1,1,1) #the shape of corr : [75x25,2, 9, 9]
+        # spt_feats_proj = self.proj(spt_feats).unsqueeze(1).unsqueeze(2).repeat(1, 1, self.vit_dim, 1) #[75x25,2,9,3]
+        # qry_feats_proj = self.proj(qry_feats).unsqueeze(1).unsqueeze(2).repeat(1, 1, self.vit_dim, 1) #[75x25,2,9,3]
+        #
+        # refined_corr = self.decoder(corr, spt_feats_proj, qry_feats_proj).view(num_qry,way,*[self.feature_size]*4)
+        # corr_s = refined_corr.view(num_qry, way, self.feature_size*self.feature_size, self.feature_size*self.feature_size)
+        # corr_q = refined_corr.view(num_qry, way, self.feature_size*self.feature_size, self.feature_size*self.feature_size)
+        # # normalizing the entities for each side to be zero-mean and unit-variance to stabilize training
+        # corr_s = self.gaussian_normalize(corr_s, dim=2)
+        # corr_q = self.gaussian_normalize(corr_q, dim=3)
+        #
+        # # applying softmax for each side
+        # corr_s = F.softmax(corr_s / self.args.temperature_attn, dim=2)
+        # corr_q = F.softmax(corr_q / self.args.temperature_attn, dim=3)
+        #
+        # # suming up matching scores
+        # attn_s = corr_s.sum(dim=[3])
+        # attn_q = corr_q.sum(dim=[2])
+        #
+        # # applying attention
+        # spt_attended = attn_s * spt_feats.view(num_qry, way, *spt_feats.shape[1:]) #[75, 25, 9]
+        # qry_attended = attn_q * qry_feats.view(num_qry, way, *qry_feats.shape[1:]) #[75, 25, 9]
 
         # ----------------------------------cat--------------------------------------#
 
-        # spt_attended = spt.unsqueeze(0).repeat(num_qry, 1, 1)
-        # qry_attended = qry.unsqueeze(1).repeat(1, way, 1)
+        spt_attended = spt.unsqueeze(0).repeat(num_qry, 1, 1)
+        qry_attended = qry.unsqueeze(1).repeat(1, way, 1)
 
         # ----------------------------------replace--------------------------------------#
 
