87463ed18ddda54bee268529bb624ea125b55f05
DRGreat <297772967@qq.com>
2024-01-14 17:05:49 +0800

    m

--- models/method.py
+++ models/method.py
@@ -34,7 +34,7 @@ class Method(nn.Module):
         self.mode = mode
         self.args = args
 
-        vit_dim = feature_size
+        vit_dim = feature_size**2
         self.vit_dim = vit_dim
         hyperpixel_ids = args.hyperpixel_ids
         self.encoder = vit_small(patch_size=16, return_all_tokens=True)
