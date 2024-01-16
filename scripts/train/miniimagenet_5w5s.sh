b3655a4b4f1f32b73e8b70a81264e310dd54b8db
DRGreat <297772967@qq.com>
2024-01-16 13:13:41 +0800

    m

--- scripts/train/miniimagenet_5w5s.sh
+++ scripts/train/miniimagenet_5w5s.sh
@@ -1,3 +1,3 @@
-python train.py -batch 128 -dataset miniimagenet -gpu 3 -extra_dir your_run -temperature_attn 5.0 -lamb 0.25 -shot 5 -no_wandb -lr 0.01 -max_epoch 10 -milestones 4 6 8
+python train.py -batch 128 -dataset miniimagenet -gpu 2 -extra_dir your_run -temperature_attn 5.0 -lamb 0.25 -shot 5 -no_wandb -lr 0.01 -max_epoch 10 -milestones 4 6 8
 
 nohup python train.py -batch 128 -dataset miniimagenet -gpu 2 -extra_dir your_run -temperature_attn 5.0 -lamb 0.25 -shot 5 -no_wandb -lr 0.01 -max_epoch 10 -milestones 4 6 8 &
